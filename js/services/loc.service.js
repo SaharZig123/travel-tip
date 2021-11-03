e27558bae769d8180984b459f0dd6708f657f2d0
Sahar zigdon <saharzigdon123@gmail.com>
2021-11-03 16:07:04 +0200

    requirement 3

--- js/services/loc.service.js
+++ js/services/loc.service.js
@@ -3,10 +3,7 @@ export const locService = {
 }
 
 // {id, name, lat, lng, weather, createdAt, updatedAt}
-const locs = [
-    { name: 'Greatplace', lat: 32.047104, lng: 34.832384 }, 
-    { name: 'Neveragain', lat: 32.047201, lng: 34.832581 }
-]
+const locs = []
 
 function getLocs() {
     return new Promise((resolve, reject) => {
